b9599c23f27ddcf870c98b9f9911ae9529fff98d
OeMAy <oemu.ayar@gmail.com>
2024-02-11 00:52:45 +0100

    Initial commit

---
+++ src/main/kotlin/oemer/aytteamforge/AytTeamforgeApplication.kt
@@ -0,0 +1,11 @@
+package oemer.aytteamforge
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AytTeamforgeApplication
+
+fun main(args: Array<String>) {
+	runApplication<AytTeamforgeApplication>(*args)
+}
