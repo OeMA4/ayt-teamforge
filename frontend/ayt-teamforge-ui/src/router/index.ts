846bf07a23528d7a302d4c0ca836eac5c9a5d321
OeMAy <oemu.ayar@gmail.com>
2024-02-13 15:31:26 +0100

    remove unnecassary routes

--- frontend/ayt-teamforge-ui/src/router/index.ts
+++ frontend/ayt-teamforge-ui/src/router/index.ts
@@ -6,25 +6,9 @@
 
 // Composables
 import { createRouter, createWebHistory } from 'vue-router/auto'
-import PlayerRankings from "@/pages/PlayerRankings.vue";
-import Home from "@/components/Home.vue";
-
-const routes = [
-  {
-    path: "/playerRankings",
-    name: "playerrankings",
-    component: PlayerRankings,
-  },
-  {
-    path: "/",
-    name: "Home",
-    component: Home,
-  },
-];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
 })
 
 export default router
