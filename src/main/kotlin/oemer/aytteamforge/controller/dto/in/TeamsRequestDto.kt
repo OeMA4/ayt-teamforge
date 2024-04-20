25c6c1721989cd0b447ea15308e0091d31d4ea0a
OemerA <oemu.ayar@gmail.com>
2024-04-20 15:57:23 +0200

    Fix Controller with correct annotation and tell jackson how to deserialize

--- src/main/kotlin/oemer/aytteamforge/controller/dto/in/TeamsRequestDto.kt
+++ src/main/kotlin/oemer/aytteamforge/controller/dto/in/TeamsRequestDto.kt
@@ -1,5 +1,9 @@
 package oemer.aytteamforge.controller.dto.`in`
 
-data class TeamsRequestDto(val players: List<PlayerDto>)
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonProperty
+
+data class TeamsRequestDto @JsonCreator constructor(
+    @JsonProperty("players") val players: List<PlayerDto>)
 
 data class PlayerDto(val name: String, val skill: Double?)
